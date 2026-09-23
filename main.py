a04fcf2db70c0e1d9151ca4f60d7eeebce5873af
Mohammad Mahroof Abdul Waheed <mohammadmahroof29@gmail.com>
2026-09-23 14:23:50 +0530

    feat: generate personalized letters in output directory

--- main.py
+++ main.py
@@ -2,7 +2,13 @@ with open("Input/Names/invited_names.txt") as file:
     names = [name.strip() for name in file.readlines()]
     
 with open("Input/Letters/starting_letter.txt") as file:
-    personalized_letter = file.read()
+    letter = file.read()
+    for name in names:
+        personalized_letter = letter.replace("[name]", name)
+        with open("Output/ReadyToSend/" + name + ".txt", "w") as file:
+            file.write(personalized_letter)
+
+
 
 
 
