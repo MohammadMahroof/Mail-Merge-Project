97b0e01413550fa60f1aceee3e7e8755af33b870
Mohammad Mahroof Abdul Waheed <mohammadmahroof29@gmail.com>
2026-09-23 14:16:58 +0530

    feat: read starting letter template from input file

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 with open("Input/Names/invited_names.txt") as file:
     names = [name.strip() for name in file.readlines()]
     
-
-
+with open("Input/Letters/starting_letter.txt") as file:
+    personalized_letter = file.read()
 
 
 
